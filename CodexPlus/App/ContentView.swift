38a73e0136b1aeb1f2f2cda2bd4eb2397f24ccaa
ana <ana@MacBook-Air-de-ana.local>
2025-09-11 02:50:21 -0300

    refector/ maintabview modificada e buttonbar excluida

--- CodexPlus/App/ContentView.swift
+++ CodexPlus/App/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                         await loadFeed()
                     }
                 }
-                ButtonBar()
+      
             }
             .ignoresSafeArea(edges: .bottom)
         }
